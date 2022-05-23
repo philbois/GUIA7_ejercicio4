9d6e247d84345d72b8274801178926d81f585c4f
nicolas philbois <102417634+philbois@users.noreply.github.com>
2022-05-22 22:28:39 -0300

    Agregar archivos de proyecto.

---
+++ Muestra.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIA7_ejercicio4
+{
+    internal class Muestra
+    {
+        int maximo, minimo, cont, acum;
+        double promedio;
+        //Cantidad de muestras, Máximo Valor, Mínimo Valor, promedio.
+        public void Ingreso(int num, int cont)
+        {
+            this.cont = cont;
+            if (acum == 0) minimo = num;
+            acum += num;
+            if (num<minimo) minimo = num;
+            if (num>maximo) maximo = num;
+           
+        }
+        public double Promedio()
+        {
+            promedio = acum * 1.0 / cont * 1.0;
+            return promedio;
+        }
+        public int Maximo()
+        {
+            return maximo;
+        }
+        public int Minimo()
+        {
+            return minimo;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*ENUNCIADO
+ Modele una clase llamada Muestra que permita procesar una serie de datos ingresados informando:
+Cantidad de muestras, Máximo Valor, Mínimo Valor, promedio.
+Utilice esta clase en una aplicación que permita cargar un número N de valores numéricos determinado al inicio por el usuario.
+*/
+namespace GUIA7_ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Muestra muestra = new Muestra();
+            int num1, num;
+            Console.WriteLine("ingrese cantidad de numeros");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <num1 ; i++)
+            {
+                num=random.Next(10,150);
+                muestra.Ingreso(num, num1);
+
+            }
+        }
+    }
+}
